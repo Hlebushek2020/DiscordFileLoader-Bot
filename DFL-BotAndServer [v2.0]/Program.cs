ac100146916b56f86a08d51490333dff44e503f3
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2021-06-20 13:52:28 +0300

    Update Init Branch v2.0

---
+++ DFL-BotAndServer [v2.0]/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DFL_BotAndServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
